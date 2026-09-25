96937f5ceb09ba6affd8a7a4956c8da1a6519021
iago <iago@iago-IdeaPad-Slim-3-15IRH10>
2026-09-25 10:12:13 +0200

    aldaketa2

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,5 +1,5 @@
 package edu.ncsu.monopoly;
-//Añadimos comentario de nuevo hoy jueves 24
+//Añadimos comentario de nuevo hoy jueves 24 aldaketa 2
 public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
